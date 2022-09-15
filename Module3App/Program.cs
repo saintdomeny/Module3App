8149aa62bcb9a6e630479872ecdf52042511ca72
Nikula Daniil <daniil.nikula@gmail.com>
2022-09-15 19:27:52 +0300

    Добавьте файлы проекта.

---
+++ Module3App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module3App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
