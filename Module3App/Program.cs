c9fdb55c000a08ab03716e8bc7a0e9a88f4c09fe
Nikula Daniil <daniil.nikula@gmail.com>
2022-09-16 13:34:47 +0300

    Questions added

--- Module3App/Program.cs
+++ Module3App/Program.cs
@@ -4,10 +4,16 @@
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Dan";
-            Console.WriteLine(myName);
-
+            string myName = "Dan";
+            int age = 27;
+            bool hasAPet = true;
+            double shoeSize = 44.5;
+            Console.WriteLine();
+            Console.WriteLine("Привет, {0}!",myName);
+            Console.WriteLine("Йоу \x23");
+            Console.WriteLine("Мой возраст {0}", age);
+            Console.WriteLine("У меня есть питомец? {0}", hasAPet);
+            Console.WriteLine("Мой размер ноги  - {0}", shoeSize);
             Console.ReadKey();
         }
     }
